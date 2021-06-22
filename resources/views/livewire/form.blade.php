115c68b6ea3afc62a44f590513d2c6ae346a774b
Gerbes Matos <gerbes.matos@gmail.com>
2021-06-21 23:50:28 -0300

    fix size

--- resources/views/livewire/form.blade.php
+++ resources/views/livewire/form.blade.php
@@ -20,9 +20,9 @@
                         <label  class="p-3 rounded border border-neutral transition-all hover:bg-gray-700 @if(${\Illuminate\Support\Str::camel(strtolower($filter['name']))}==$settings['code']) bg-neutral @endif" >
                             <input class="form-check-input mr-2" wire:model="{{ \Illuminate\Support\Str::camel(strtolower($filter['name'])) }}" name="{{ md5($filter['id']) }}" type="radio" value="{{ $settings['code'] }}">
                             @if($filter['name']=='Players')
-                                <span class="form-check-label text-xs md:text-md  @if(${\Illuminate\Support\Str::camel(strtolower($filter['name']))}==$settings['code']) text-white font-bold @else text-gray-600 @endif"> {{ $settings['name']  }} Players</span>
+                                <span class="form-check-label text-white  md:text-md  @if(${\Illuminate\Support\Str::camel(strtolower($filter['name']))}==$settings['code']) text-white font-bold @else text-gray-600 @endif"> {{ $settings['name']  }} Players</span>
                             @else
-                                <span class="form-check-label text-xs md:text-md  @if(${\Illuminate\Support\Str::camel(strtolower($filter['name']))}==$settings['code']) text-white font-bold @else text-gray-600  @endif"> {{ __('app.'.$settings['name']) }} </span>
+                                <span class="form-check-label md:text-md  @if(${\Illuminate\Support\Str::camel(strtolower($filter['name']))}==$settings['code']) text-white font-bold @else text-gray-600  @endif"> {{ __('app.'.$settings['name']) }} </span>
                             @endif
                         </label>
 
